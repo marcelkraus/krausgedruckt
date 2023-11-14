7dead4a3016a7aed2299a5043f620a58a022c575
Marcel Kraus <git@marcelkraus.de>
2023-11-14 10:01:57 +0100

    Add first bunch of content to home screen

--- components/app/tailwind.config.js
+++ components/app/tailwind.config.js
@@ -1,8 +1,24 @@
-/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
 module.exports = {
   content: ["./templates/**/*.html.twig"],
   theme: {
-    extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      orange: colors.orange,
+      gray: colors.neutral,
+    },
+    extend: {
+      colors: {
+        brand: {
+          primary: '#f97316', // Orange:400
+          secondary: '#4b5563', // Gray:600
+        },
+        primary: '#111827', // Gray:900
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/forms'),
