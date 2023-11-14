ecf70f090d1109002726b1eed374d082bb4d6fc2
Marcel Kraus <git@marcelkraus.de>
2023-11-14 18:12:21 +0100

    Add (dummy) contact form with additional structural changes

--- components/app/tailwind.config.js
+++ components/app/tailwind.config.js
@@ -7,15 +7,17 @@ module.exports = {
       current: 'currentColor',
       black: colors.black,
       white: colors.white,
-      orange: colors.orange,
-      gray: colors.neutral,
     },
     extend: {
       colors: {
+        background: {
+          primary: '#ffedd5' // Orange:100
+        },
         brand: {
           primary: '#f97316', // Orange:400
           secondary: '#4b5563', // Gray:600
         },
+        placeholder: '#9ca3af', // Gray:400
         primary: '#111827', // Gray:900
       },
     },
