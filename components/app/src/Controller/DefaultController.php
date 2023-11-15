db8a01af4de7383acffd4293eee0ab15d67df430
Marcel Kraus <git@marcelkraus.de>
2023-11-15 18:06:45 +0100

    Add logic for contact form

--- components/app/src/Controller/DefaultController.php
+++ components/app/src/Controller/DefaultController.php
@@ -2,16 +2,51 @@
 
 namespace App\Controller;
 
+use App\Entity\ContactRequest;
+use App\Form\Type\ContactRequestType;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
     #[Route('/', name: 'app_homepage')]
-    public function index(): Response
+    public function index(Request $request, MailerInterface $mailer): Response
     {
-        return $this->render('content/homepage.html.twig');
+        $form = $this->createForm(ContactRequestType::class, new ContactRequest());
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $contactRequest = $form->getData();
+            $message = (new TemplatedEmail())
+                ->from($_SERVER['CONTACT_FORM_SENDER_ADDRESS'])
+                ->to($_SERVER['CONTACT_FORM_RECIPIENT_ADDRESS'])
+                ->replyTo($contactRequest->getEmail())
+                ->subject('Neue Kontaktanfrage erhalten')
+                ->textTemplate('email/contact.txt.twig')
+                ->context([
+                    'name' => $contactRequest->getName(),
+                    'emailAddress' => $contactRequest->getEmail(),
+                    'message' => $contactRequest->getMessage(),
+                ]);
+
+            $mailer->send($message);
+
+            return $this->redirectToRoute('app_contact_confirmation');
+        }
+
+        return $this->render('content/homepage.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    #[Route('/vielen-dank-fuer-deine-anfrage', name: 'app_contact_confirmation')]
+    public function contactConfirmation(): Response
+    {
+        return $this->render('content/contact-confirmation.html.twig');
     }
 
     #[Route('/impressum', name: 'app_imprint')]
---
+++ components/app/src/Form/Type/ContactRequestType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Form\Type;
+
+use App\Entity\ContactRequest;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ContactRequestType extends AbstractType {
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $form = $builder
+            ->add('name', TextType::class, [
+                'label' => false,
+                'attr' => ['placeholder' => 'Dein Name'],
+            ])
+            ->add('email', EmailType::class, [
+                'label' => false,
+                'attr' => ['placeholder' => 'Deine E-Mail-Adresse'],
+            ])
+            ->add('message', TextareaType::class, [
+                'label' => false,
+                'attr' => [
+                    'rows' => 5,
+                    'placeholder' => 'Deine Nachricht',
+                ],
+            ])
+        ;
+
+        $form->add('submit', SubmitType::class, [
+            'label' => 'Anfrage senden'
+        ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => ContactRequest::class,
+        ]);
+    }
+}
