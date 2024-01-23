9b3d119ef8ca3dd17b20140e64f46c49c0d08d48
Marcel Kraus <git@marcelkraus.de>
2024-01-23 16:21:07 +0100

    Simplify handling of references and images for references

--- src/Entity/Reference.php
+++ src/Entity/Reference.php
@@ -2,35 +2,26 @@
 
 namespace App\Entity;
 
-use \DateTimeImmutable;
+use Symfony\Component\Finder\Finder;
 
 class Reference
 {
     protected int $id;
-    protected DateTimeImmutable $date;
     protected string $title;
     protected array $image;
-    protected ?string $client;
-
     protected string $description;
     protected ?Source $source;
 
     /**
      * @param int $id
-     * @param string $date
      * @param string $title
-     * @param array $images
-     * @param ?string $client
      * @param string $description
      * @param ?Source $source
      */
-    public function __construct(int $id, string $date, string $title, array $images, string $client = null, string $description, ?Source $source = null)
+    public function __construct(int $id, string $title, string $description, ?Source $source = null)
     {
         $this->id = $id;
-        $this->date = DateTimeImmutable::createFromFormat("Y-m-d", $date);
         $this->title = $title;
-        $this->images = $images;
-        $this->client = $client;
         $this->description = $description;
         $this->source = $source;
     }
@@ -40,11 +31,6 @@ class Reference
         return $this->id;
     }
 
-    public function getDate(): DateTimeImmutable
-    {
-        return $this->date;
-    }
-
     public function getTitle(): string
     {
         return $this->title;
@@ -52,12 +38,19 @@ class Reference
 
     public function getImages(): array
     {
-        return $this->images;
-    }
+        $result = [];
 
-    public function getClient(): ?string
-    {
-        return $this->client;
+        $finder = new Finder();
+        $finder
+            ->files()
+            ->name('*.jpg')
+            ->in(sprintf('%s/../../public/images/references/%d', __DIR__ , $this->id));
+
+        foreach ($finder as $file) {
+            $result[] = $file->getRelativePathname();
+        }
+
+        return $result;
     }
 
     public function getDescription(): string
