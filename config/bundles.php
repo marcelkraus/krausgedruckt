af0369b80c07e1068ed0aa273a5e3a10b2aa8c3f
Marcel Kraus <git@marcelkraus.de>
2025-07-02 21:06:55 +0200

    Install Omines Anti-Spam Bundle

--- config/bundles.php
+++ config/bundles.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+    Omines\AntiSpamBundle\AntiSpamBundle::class => ['all' => true],
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
